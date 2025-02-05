3cfc4ce3e3cb0c24f25796fbda414e1dbdccc081
Raul Luna <mceclipse1889@gmail.com>
2025-02-05 16:09:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* Mobile Navbar */
 const HEADER_BARS = document.querySelector("#header-bars");
 const MOBILE_NAV = document.querySelector("#mobile-nav");
 const MOBILE_LINKS = document.querySelectorAll(".mobile-link");
@@ -23,3 +24,25 @@ MOBILE_LINKS.forEach(link => {
         document.body.style.overflowY = "auto";
     });
 });
+
+/* Change Theme */
+const THEME_TOGGLE_BTNS = document.querySelectorAll(".theme-toggle");
+
+const CURRENT_THEME = localStorage.getItem("theme");
+if (CURRENT_THEME === "dark") {
+    document.body.classList.add("dark-mode");
+}
+
+THEME_TOGGLE_BTNS.forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+
+        if (document.body.classList.contains("dark-mode")) {
+            localStorage.setItem("theme", "dark");
+        } else {
+            localStorage.removeItem("theme", "dark");
+        }
+
+        document.body.style.transition = "background-color .2s ease-in-out";
+    });
+}); 
